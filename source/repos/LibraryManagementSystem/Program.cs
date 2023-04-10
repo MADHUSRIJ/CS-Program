664e72a79bc1f0327d1b3174d2e480049f498c26
Madhusri J <83413392+MADHUSRIJ@users.noreply.github.com>
2023-04-10 16:43:34 +0530

    Initial Commit

---
+++ source/repos/LibraryManagementSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SqlProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=5CG9441HWP;Initial Catalog=Library;Integrated Security=True;Encrypt=False;");
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+
+            
+
+        }
+    }
+}
